2777c93629c41813a3d00fa546b8b66cead25ef9
Ali Hammoud <ali.hammoud.lb@gmail.com>
2025-02-11 01:28:55 +0300

    Optimized canvas size

--- js/conway.js
+++ js/conway.js
@@ -7,8 +7,8 @@ let mainCanvas;
 
 function setup() {
   // Set simulation framerate to 10 to avoid flickering
-  frameRate(15);
-  mainCanvas = createCanvas((window.screen.width * 0.57) / 2, (window.screen.height * 0.57) / 2);
+  frameRate(10);
+  mainCanvas = createCanvas((window.screen.width * 0.57), (window.screen.height * 0.57));
   mainCanvas.parent("p5canvas");
   document.querySelector('.p5Canvas').addEventListener('click', spawnGlider)
 
@@ -73,7 +73,8 @@ function randomizeBoard() {
   for (let column = 0; column < columnCount; column++) {
     for (let row = 0; row < rowCount; row++) {
       // Randomly select value of either 0 (dead) or 1 (alive)
-      currentCells[column][row] = random([0, 1]);
+      //currentCells[column][row] = random([0, 1]);
+      currentCells[column][row] = round(noise(row, column)*1)
     }
   }
 }
